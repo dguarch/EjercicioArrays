6795e307fe9b7400ce43bdb9f3efbdd44a0c4320
A4Alumno <a4alumno@aules.local>
2018-04-10 15:29:32 +0200

    Programa modificado

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let rangoNumeros = [10, 20, 30, 40];
-let numero = 27;
-if (numero < rangoNumeros[0]) {
+let numero;
+
+numero = prompt('Dime un numero del 0 al 100:');
+
+if (numero == 10) {
+    console.log(`El número que has elegido es ${numero}.`);
+} else if (numero == 20) {
+    console.log(`El número que has elegido es ${numero}.`);
+} else if (numero == 30) {
+    console.log(`El número que has elegido es ${numero}.`);
+} else if (numero == 40) {
+    console.log(`El número que has elegido es ${numero}.`);
+} else if (numero < rangoNumeros[0]) {
     console.log(`${numero} es menor que ${rangoNumeros[0]}.`);
 } else if (numero >= rangoNumeros[0] && numero < rangoNumeros[1]) {
     console.log(`${numero} es mayor que ${rangoNumeros[0]}, pero más pequeño que ${rangoNumeros[1]}.`);
