ba8d3614e9325889233ed812845f573ca7f976de
A4Alumno <a4alumno@aules.local>
2018-04-10 15:52:55 +0200

    Modificación del programa

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let rangoNumeros = [10, 20, 30, 40];
+let rangoNumero = [10, 20, 30, 40];
 let numero;
 
 numero = prompt('Dime un numero del 0 al 100:');
 
-if (numero == 10) {
+if (numero == rangoNumero[0]) {
     console.log(`El número que has elegido es ${numero}.`);
-} else if (numero == 20) {
+} else if (numero == rangoNumero[1]) {
     console.log(`El número que has elegido es ${numero}.`);
-} else if (numero == 30) {
+} else if (numero == rangoNumero[2]) {
     console.log(`El número que has elegido es ${numero}.`);
-} else if (numero == 40) {
+} else if (numero == rangoNumero[3]) {
     console.log(`El número que has elegido es ${numero}.`);
-} else if (numero < rangoNumeros[0]) {
-    console.log(`${numero} es menor que ${rangoNumeros[0]}.`);
-} else if (numero >= rangoNumeros[0] && numero < rangoNumeros[1]) {
-    console.log(`${numero} es mayor que ${rangoNumeros[0]}, pero más pequeño que ${rangoNumeros[1]}.`);
-} else if (numero >= rangoNumeros[1] && numero < rangoNumeros[2]) {
-    console.log(`${numero} es mayor que ${rangoNumeros[1]}, pero más pequeño que ${rangoNumeros[2]}.`);
-} else if (numero >= rangoNumeros[2] && numero < rangoNumeros[3]) {
-    console.log(`${numero} es mayor que ${rangoNumeros[2]}, pero más pequeño que ${rangoNumeros[3]}.`)
+} else if (numero < rangoNumero[0]) {
+    console.log(`${numero} es menor que ${rangoNumero[0]}.`);
+} else if (numero >= rangoNumero[0] && numero < rangoNumero[1]) {
+    console.log(`${numero} es mayor que ${rangoNumero[0]}, pero más pequeño que ${rangoNumero[1]}.`);
+} else if (numero >= rangoNumero[1] && numero < rangoNumero[2]) {
+    console.log(`${numero} es mayor que ${rangoNumero[1]}, pero más pequeño que ${rangoNumero[2]}.`);
+} else if (numero >= rangoNumero[2] && numero < rangoNumero[3]) {
+    console.log(`${numero} es mayor que ${rangoNumero[2]}, pero más pequeño que ${rangoNumero[3]}.`)
 } else {
-    console.log(`${numero} es mayor que ${rangoNumeros[3]}.`)
+    console.log(`${numero} es mayor que ${rangoNumero[3]}.`)
 }
